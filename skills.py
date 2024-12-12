e984f470f4a514882b528f50fd659392af80da5c
imDarshanGK <darshan241210s@gmail.com>
2024-12-12 22:10:28 +0530

    Copilot first commit

--- skills.py
+++ skills.py
@@ -1,2 +1,2 @@
-def calculateNumbers(var11, var2):     
-    return var11 + var2        
+def calculateNumbers(var1, var2): # Function to calculate the sum of two numbers                            
+    return var1 + var2        
